45a1d741851ff3483ecc52f23d9688a20c4d4c1d
prasannaa09 <prasanna.alur.09@gmail.com>
2025-01-22 15:56:03 +0530

    Updated BloodGroup.java enum

--- src/main/java/com/example/bsm/enums/BloodGroup.java
+++ src/main/java/com/example/bsm/enums/BloodGroup.java
@@ -1,4 +1,4 @@
-package com.example.bsm.entity;
+package com.example.bsm.enums;
 
 public enum BloodGroup {
     A_POSITIVE, A_NEGATIVE, B_POSITIVE, B_NEGATIVE, O_POSITIVE, O_NEGATIVE, AB_POSITIVE, AB_NEGATIVE
