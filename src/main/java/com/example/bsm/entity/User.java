a460548e3438b4795baf04acc09b909975ff41af
prasannaa09 <prasanna.alur.09@gmail.com>
2025-01-22 15:55:27 +0530

    Updated User.java entity with all the necessary input

--- src/main/java/com/example/bsm/entity/User.java
+++ src/main/java/com/example/bsm/entity/User.java
@@ -1,4 +1,47 @@
 package com.example.bsm.entity;
 
+import com.example.bsm.enums.BloodGroup;
+import com.example.bsm.enums.Gender;
+import com.example.bsm.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    private String username;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroup bloodGroup;
+
+    private LocalDate lastDonatedAt;
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private String availableCity;
+    private boolean verified;
+
+
+
 }
