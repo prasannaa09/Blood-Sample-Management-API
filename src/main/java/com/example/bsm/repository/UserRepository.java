c31f43e276bf2d8f2c6a66766c4647905492a40a
prasannaa09 <prasanna.alur.09@gmail.com>
2025-01-22 15:57:21 +0530

    Updated UserRepository.java

--- src/main/java/com/example/bsm/repository/UserRepository.java
+++ src/main/java/com/example/bsm/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.example.bsm.repository;
 
-public class UserRepository {
+import com.example.bsm.entity.User;
+import com.example.bsm.enums.BloodGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserId(Long userId);
 }
