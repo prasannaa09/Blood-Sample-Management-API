68409453ce538165ace38482425109c55c33e5d2
prasannaa09 <prasanna.alur.09@gmail.com>
2025-01-22 15:57:36 +0530

    Updated UserService.java

--- src/main/java/com/example/bsm/service/UserService.java
+++ src/main/java/com/example/bsm/service/UserService.java
@@ -1,4 +1,51 @@
 package com.example.bsm.service;
 
+import com.example.bsm.entity.User;
+import com.example.bsm.exception.UserNotFoundException;
+import com.example.bsm.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    // Create
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Read
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    // Update
+    public User updateUser(Long userId, User updatedUser) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setUsername(updatedUser.getUsername());
+            user.setEmail(updatedUser.getEmail());
+            user.setRole(updatedUser.getRole());
+            user.setPhoneNumber(updatedUser.getPhoneNumber());
+
+            return userRepository.save(user);
+        } else {
+            throw new RuntimeException("User not found");
+        }
+    }
+
+    // Delete
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+
 }
