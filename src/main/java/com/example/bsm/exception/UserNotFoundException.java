0b9287de1a93b61dc09bd4d51fb512042d345077
prasannaa09 <prasanna.alur.09@gmail.com>
2025-01-22 15:57:04 +0530

    Updated UserNotFoundException.java

--- src/main/java/com/example/bsm/exception/UserNotFoundException.java
+++ src/main/java/com/example/bsm/exception/UserNotFoundException.java
@@ -1,4 +1,7 @@
-package com.example.bsm.service;
+package com.example.bsm.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
